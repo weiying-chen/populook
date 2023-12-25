2f606ffc54107304ce0e696a70752c26aa73d8fa
Wei-ying Chen <weiying.alex.chen@gmail.com>
2023-12-25 22:37:37 +0800

    Add types

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,68 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState, useEffect, ChangeEvent } from 'react';
+import './App.css';
+import axios from 'axios';
 
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+interface City {
+  rank: number;
+  city: string;
+  state: string;
+  population: string;
 }
 
-export default App
+interface SearchInputProps {
+  value: string;
+  onChange: (event: ChangeEvent<HTMLInputElement>) => void;
+}
+
+function SearchInput({ value, onChange }: SearchInputProps) {
+  return <input type="text" value={value} onChange={onChange} placeholder="City or State" />;
+}
+
+interface SuggestionsListProps {
+  cities: City[];
+}
+
+function SuggestionsList({ cities }: SuggestionsListProps) {
+  return (
+    <ul>
+      {cities.map(city => (
+        <li key={city.rank}>
+          <span className="name">{city.city}, {city.state}</span>
+          <span className="population">{city.population}</span>
+        </li>
+      ))}
+    </ul>
+  );
+}
+
+function App() {
+  const [cities, setCities] = useState<City[]>([]);
+  const [searchTerm, setSearchTerm] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const result = await axios('https://gist.githubusercontent.com/Miserlou/c5cd8364bf9b2420bb29/raw/2bf258763cdddd704f8ffd3ea9a3e81d25e2c6f6/cities.json');
+      setCities(result.data);
+    };
+
+    fetchData();
+  }, []);
+
+  const handleSearch = (event: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(event.target.value);
+  };
+
+  const filteredCities = cities.filter(city =>
+    city.city.toLowerCase().includes(searchTerm.toLowerCase()) || 
+    city.state.toLowerCase().includes(searchTerm.toLowerCase())
+  );
+
+  return (
+    <div className="App">
+      <SearchInput value={searchTerm} onChange={handleSearch} />
+      <SuggestionsList cities={filteredCities} />
+    </div>
+  );
+}
+
+export default App;
